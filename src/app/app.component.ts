83f8d0f280deaf58e5458bf83f9b3095ea4e7d10
180701127 <logkum31052000@gmail.com>
2023-06-28 21:27:57 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { CurrencyService } from './services/currency.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
+  
   title = 'crypto-checker';
+  selectedCurrency: string = "INR";
+  constructor(private currencyService: CurrencyService){}
+
+  sendCurrency(event:string){
+    console.log(event);
+    this.currencyService.setCurrency(event);
+  }
 }
